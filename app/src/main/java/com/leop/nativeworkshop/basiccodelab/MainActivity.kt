e93de5beba8f6d5a4305d8de47a85a826a5d9f3e
leodegario.pasakdal <leodegario.pasakdal@wolt.com>
2026-03-03 14:26:09 +0100

    state in compose

--- app/src/main/java/com/leop/nativeworkshop/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/leop/nativeworkshop/basiccodelab/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,6 +41,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    val expanded = remember { mutableStateOf(false) }
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical=4.dp, horizontal = 8.dp)
@@ -48,8 +53,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text( text= "Hell")
                 Text ( text = name)
             }
-            ElevatedButton(onClick = {}) {
-                Text( text = "Show more")
+            ElevatedButton(onClick = { expanded.value = !expanded.value}) {
+                Text( if (expanded.value) "Show more" else "Show less" )
             }
         }
 
