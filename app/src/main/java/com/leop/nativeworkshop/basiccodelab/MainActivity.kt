ca5a60cd9d0039a6055b24b4f0a611bedb1e8c52
leodegario.pasakdal <leodegario.pasakdal@wolt.com>
2026-03-03 14:12:27 +0100

    codelab creating columns and rows

---
+++ app/src/main/java/com/leop/nativeworkshop/basiccodelab/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.leop.nativeworkshop.basiccodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.leop.nativeworkshop.basiccodelab.ui.theme.BasicCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BasicCodelabTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical=4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text( text= "Hell")
+                Text ( text = name)
+            }
+            ElevatedButton(onClick = {}) {
+                Text( text = "Show more")
+            }
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingPreview() {
+    BasicCodelabTheme {
+        MyApp()
+//        Greeting("here")
+    }
+
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier,
+          names : List<String> = listOf("name1", "name2")) {
+        Column(modifier) {
+            for (name in names) {
+                Greeting(name = name)
+            }
+        }
+}
